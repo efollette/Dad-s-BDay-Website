8fc31bbf04b9d5987b15002827e8395e12fa8a16
Erik Follette <erikfollette@fastmail.fm>
2020-02-11 15:16:58 -0800

    Sending this to the cloud!

---
+++ dad.js
@@ -0,0 +1,14 @@
+// Wrap every letter in a span
+var textWrapper = document.querySelector('.ml2');
+textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+
+anime.timeline({})
+  .add({
+    targets: '.ml2 .letter',
+    scale: [4,1],
+    opacity: [0,1],
+    translateZ: 0,
+    easing: "easeOutExpo",
+    duration: 950,
+    delay: (el, i) => 70*i
+  })
